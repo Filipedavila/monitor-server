192fb27134fbc2b9e3d6668dc26280b43d0b6ec4
Filipe Dávila <filipedavila6@gmail.com>
2026-05-01 21:58:43 +0100

    feat(bootstrap): enhance security and logging setup, add versioning and validation pipes

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,32 @@ import { AppModule } from "./app.module";
 import express from "express";
 import helmet from "helmet";
 import compression from "compression";
-import { WINSTON_MODULE_NEST_PROVIDER } from "nest-winston";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import { HttpExceptionFilter } from "./lib/http-exception.filter";
+import { IoAdapter } from "@nestjs/platform-socket.io";
+import { ValidationPipe, VersioningType } from "@nestjs/common";
+import { AppLoggerService } from "./core/app-logger/app-logger.service";
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.use(helmet());
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: [`'self'`],
+          styleSrc: [`'self'`, `'unsafe-inline'`],
+          imgSrc: [`'self'`, "data:", "validator.swagger.io"],
+          scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+        },
+      },
+    }),
+  );
+  app.useWebSocketAdapter(new IoAdapter(app) as any);
   app.use(express.json({ limit: "50mb" }));
   app.use(express.urlencoded({ limit: "50mb", extended: true }));
   app.use(compression());
-  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+  const logger = await app.resolve(AppLoggerService);
+  app.useLogger(logger);
 
   const config = new DocumentBuilder()
     .setTitle("Monitor server")
@@ -22,8 +37,24 @@ async function bootstrap() {
     .addTag("website")
     .build();
   const document = SwaggerModule.createDocument(app, config);
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: "2",
+  });
   SwaggerModule.setup("api", app, document);
-  app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(process.env.PORT || 3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(` Server is running on: http://localhost:${port}/v2`);
+  console.log(` Swagger documentation: http://localhost:${port}/api`);
 }
 bootstrap();
