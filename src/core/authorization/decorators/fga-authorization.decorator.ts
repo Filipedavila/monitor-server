abe7ac2f6e433c634080b76a072f9a50308a43c8
Filipe Dávila <filipedavila6@gmail.com>
2026-07-10 11:46:44 +0100

    feat(authorization): implement ContextFilterGuard for role-based context filtering refactor(types): remove unused PaginationResponse interface

--- src/core/authorization/decorators/fga-authorization.decorator.ts
+++ src/core/authorization/decorators/fga-authorization.decorator.ts
@@ -1,10 +1,10 @@
 import { ExecutionContext, SetMetadata } from '@nestjs/common';
-import { FgaModelMap, ResourceType } from '../types/fga.types';
+import { EnquireRelationType, FgaTupleEnquire , ResourceType} from '../types/fga.types';
 
-export interface FgaMetadata<T extends ResourceType> {
+export interface FgtaTupleCheck<T extends ResourceType, E extends keyof EnquireRelationType> {
   objectType: T;
-  action: FgaModelMap[Extract<T, keyof FgaModelMap>];
+  action: FgaTupleEnquire<E>['relation'];
   resourceIdResolver?: (ctx: ExecutionContext) => string | undefined;
 }
 export const FGA_DECORATOR_KEY = 'fga_authorization';
-export const FgaAuthorized = <T extends ResourceType>(data: FgaMetadata<T>) => SetMetadata(FGA_DECORATOR_KEY, data);
+export const FgaAuthorized = <T extends ResourceType, E extends keyof EnquireRelationType>(data: FgtaTupleCheck<T, E>) => SetMetadata(FGA_DECORATOR_KEY, data);
